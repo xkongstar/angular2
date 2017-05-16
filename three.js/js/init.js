95b62652482558fd5b335542f21b6364977f2790
xkongstar <847551084@qq.com>
2017-05-16 18:35:13 +0800

    hello world

--- three.js/js/init.js
+++ three.js/js/init.js
@@ -4,4 +4,14 @@ function init() {
 	document.getElementsByTagName('body')[0].appendChild(renderer.domElement);
 	renderer.setClearColor(0x0000);
 	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 1000);
+	camera.position.set(0, 0, 5);
+	scene.add(camera);
+	var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 2, 3),
+		new THREE.MeshBasicMaterial({
+			color: 0xff0000
+		})
+	);
+	scene.add(cube);
+	renderer.render(scene,camera)
 }
